239e984aabe5f4b4c09c1c3aaeeb02f7646eab0c
AndyMelone <meloneandy225@gmail.com>
2024-12-12 04:41:18 +0000

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ const interventionData = [
 export default function Home() {
   return (
     <>
-      <div className="w-full">
+      <div className="w-full flex">
         <div className="w-3/5">
           <h1 className="text-2xl font-semibold text-gray-600 mt-24">
             Bienvenue sur la plateforme de gestion des urgences
@@ -101,7 +101,28 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="w-2/5"></div>
+        <div className="w-2/5 pt-24">
+          <h1 className="text-2xl font-semibold ">
+            S<span className="text-red-700">O</span>
+            S-AI
+          </h1>
+          <p className="text-lg pt-10">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio
+            earum saepe atque hic, debitis magni, tempore reiciendis voluptate,
+            rerum ad facilis blanditiis tenetur? Vitae soluta quia debitis
+            delectus vero dolorum. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laborum nisi est facilis quisquam consequatur
+            beatae, consectetur dolorem sit inventore quis, repellendus
+            provident magni tenetur nobis? Tempora vel assumenda nostrum
+            ducimus. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Dolores minima autem aut atque ratione, quo blanditiis excepturi,
+            fugit quae rem beatae pariatur enim impedit praesentium, dolorem
+            illum at error doloribus. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Quam exercitationem hic sunt accusamus. Qui
+            ratione et enim maiores neque amet vitae, architecto corporis ab
+            omnis! Eligendi nulla quibusdam magnam officiis!
+          </p>
+        </div>
       </div>
     </>
   );
