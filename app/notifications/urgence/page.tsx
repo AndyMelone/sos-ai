929865a1c73f571d43002d52659166d0986c239a
AndyMelone <meloneandy225@gmail.com>
2024-12-12 01:47:47 +0000

    init commit

--- app/notifications/urgence/page.tsx
+++ app/notifications/urgence/page.tsx
@@ -8,26 +8,35 @@ export default function Page() {
           Urgence : 🚨 Icendit de vehicule a YOP
         </h1>
       </div>
-      <p className="mt-4">
-        <span className="font-semibold">Date:</span> 2021-10-10
-      </p>
-      <p className="mt-4">
-        <span className="font-semibold">Localisation:</span> Yopougon
-      </p>
+      <div className="my-4">
+        <p className="">
+          <span className="font-semibold">Date:</span> 2021-10-10
+        </p>
+        <p className="mt-4">
+          <span className="font-semibold">Localisation:</span> Yopougon
+        </p>
+        <p className="mt-4">
+          <span className="font-semibold">Risque potenciel:</span> Risque de
+          propagation
+        </p>
+        <p className="mt-4">
+          <span className="font-semibold">
+            Contexte de l&apos;intervention:
+          </span>{" "}
+          Un vehicule a pris feu a Yopougon
+        </p>
+      </div>
 
-      <p className="mt-4">
-        <span className="font-semibold">Risque potenciel:</span> Risque de
-        propagation
-      </p>
-
-      <p className="mt-4">
-        <span className="font-semibold">Contexte de l'intervention:</span> Un
-        vehicule a pris feu a Yopougon
-      </p>
-
-      <p className="mt-4">
-        <span className="font-semibold">Appelant:</span> 225 07 07 07 07
-      </p>
+      <div className="border-b pb-4 pt-8">
+        <h1 className="text-xl font-semibold">
+          Information sur l&apos;appelant
+        </h1>
+      </div>
+      <div className="">
+        <p className="mt-4">
+          <span className="font-semibold">Appelant:</span> 225 07 07 07 07
+        </p>
+      </div>
     </>
   );
 }
