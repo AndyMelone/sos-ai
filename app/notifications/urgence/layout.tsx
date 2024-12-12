cf198bcd9aabf475e5f03787688af1d3badd1a2f
AndyMelone <meloneandy225@gmail.com>
2024-12-12 01:43:03 +0000

    init commit

--- app/notifications/urgence/layout.tsx
+++ app/notifications/urgence/layout.tsx
@@ -6,11 +6,16 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <>
+    <div className="w-full">
       <div>
         <button>valide</button>
       </div>
-      <div>{children}</div>
-    </>
+      <div className="flex">
+        <div className="w-3/5">{children}</div>
+        <div className="w-2/5 border">
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
   );
 }
--- app/notifications/urgence/page.tsx
+++ app/notifications/urgence/page.tsx
@@ -3,7 +3,31 @@ import React from "react";
 export default function Page() {
   return (
     <>
-      <p>hello</p>
+      <div className="border-b pb-4">
+        <h1 className="text-2xl font-semibold">
+          Urgence : 🚨 Icendit de vehicule a YOP
+        </h1>
+      </div>
+      <p className="mt-4">
+        <span className="font-semibold">Date:</span> 2021-10-10
+      </p>
+      <p className="mt-4">
+        <span className="font-semibold">Localisation:</span> Yopougon
+      </p>
+
+      <p className="mt-4">
+        <span className="font-semibold">Risque potenciel:</span> Risque de
+        propagation
+      </p>
+
+      <p className="mt-4">
+        <span className="font-semibold">Contexte de l'intervention:</span> Un
+        vehicule a pris feu a Yopougon
+      </p>
+
+      <p className="mt-4">
+        <span className="font-semibold">Appelant:</span> 225 07 07 07 07
+      </p>
     </>
   );
 }
